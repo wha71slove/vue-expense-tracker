115936abfe1330fb291be801a34dd321888e0c55
whatislove <v.katrich87@mail.ru>
2024-07-19 01:01:57 +0500

    Second commit: add project for GH-Pages

--- src/components/TrackerHeader.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>Учет Расходов</h2>
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
-import App from './App.vue'
-import './assets/style.css'
-
-const app = createApp(App)
-app.use(Toast)
-app.mount('#app')
