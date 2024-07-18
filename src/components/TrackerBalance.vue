c9fba44479bbae7df15efa7e02ecf4020f7b3cc0
whatislove <v.katrich87@mail.ru>
2024-07-19 00:44:04 +0500

    First commit

---
+++ src/components/TrackerBalance.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  totalAmount: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h4>Ваш баланс</h4>
+  <h1 id="balance">{{ totalAmount }} ₽</h1>
+</template>
